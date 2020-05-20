62217981e03d1f2e291863d68d77b50a641c107a
Christian Lin <linchristiands@gmail.com>
2020-05-20 22:23:15 +0900

    c

--- firebase.js
+++ firebase.js
@@ -3,15 +3,7 @@ import 'firebase/functions';
 require("firebase/firestore");
 
 
-const config = {
-    apiKey: "AIzaSyCPPUto1TtS74ALvfb6UGHNK0AYDXG-QGI",
-    authDomain: "fir-react-kadai.firebaseapp.com",
-    databaseURL: "https://fir-react-kadai.firebaseio.com",
-    projectId: "fir-react-kadai",
-    storageBucket: "fir-react-kadai.appspot.com",
-    messagingSenderId: "7538340630",
-    appId: "1:7538340630:web:c550321c0f97820ec35637"
-  };
+const config = {};
 // Initialize Firebase
 firebase.initializeApp(config);
 
